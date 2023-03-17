000ba9194c4e9da366b5bed83788239bcabc6a2e
Joan Guinart Casas <guinartcasasjoan@gmail.com>
2023-03-17 21:29:38 +0100

    viteconfig change

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://JoanGuinart.gitlab.io/Portfolio/' ,
   build: {
     chunkSizeWarningLimit: 1600,
   },
