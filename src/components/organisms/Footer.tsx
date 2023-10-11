dae093cdb79a1c76b72d9182b964204eef547072
Joan Guinart Casas <guinartcasasjoan@gmail.com>
2023-10-11 16:55:41 +0200

    feat: new footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Insta from "../../assets/insta-logo.png";
+import LinkedIn from "../../assets/linkedin.png";
+import GitHub from "../../assets/icons8-github-512.png";
+import Logo from "../../assets/logo-dev.png"
 
 const FooterWrapper = styled.footer`
   display: grid;
+  bottom: 0;
   grid-template-columns: 1fr 1fr 1fr;
   background-color: #333;
   color: #fff;
@@ -42,7 +47,7 @@ const SocialLink = styled.a`
   transition: color 0.3s ease-in-out;
 
   &:hover {
-    color: #ff8777;
+    transform: scale(0.95);
   }
 `;
 
@@ -56,13 +61,13 @@ const ContactInfo = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  margin-top: 1rem;
 `;
 
 const ContactLink = styled.a`
   display: inline-block;
   margin-bottom: 0.5rem;
   color: #fff;
+  font-weight: bold;
   text-decoration: none;
   transition: color 0.3s ease-in-out;
 
@@ -74,21 +79,36 @@ const ContactLink = styled.a`
 const Footer = () => {
   return (
     <FooterWrapper>
-      <LeftSection>Left</LeftSection>
-      {/* -------------------------------------------------------------- */}
+      <LeftSection>
+        <img style={{width: '100px', filter: 'invert(100%)'}} src={Logo} alt="" />
+      </LeftSection>
       <CenterSection>
         <Name>Joan Guinart Casas</Name>
         <SocialMedia>
-          <SocialLink>insta</SocialLink>
-          <SocialLink>linkdin</SocialLink>
-          <SocialLink>blog</SocialLink>
+          <SocialLink href="https://www.instagram.com/joanguinart_/"  target="_blank">
+            <img style={{ width: "30px" }} src={Insta} alt="logo instagram" />
+          </SocialLink>
+          <SocialLink href="https://www.linkedin.com/in/joan-guinart-casas/" target="_blank">
+            <img
+              style={{ width: "30px" }}
+              src={LinkedIn}
+              alt="logo linkedin"
+            />
+          </SocialLink>
+          <SocialLink href="https://github.com/JoanGuinart" target="_blank">
+            <img style={{ width: "30px" }} src={GitHub} alt="logo github" />
+          </SocialLink>
         </SocialMedia>
       </CenterSection>
       {/* -------------------------------------------------------------- */}
       <RightSection>
         <ContactInfo>
-          telef, mail, number
-          <ContactLink>github, portfolio</ContactLink>
+          <ContactLink href={"tel: +34617056810"}>
+            Tlf: +34 617 05 68 10
+          </ContactLink>
+          <ContactLink href={"mailto: guinartcasasjoan@gmail.com"}>
+            Email: guinartcasasjoan@gmail.com
+          </ContactLink>
         </ContactInfo>
       </RightSection>
     </FooterWrapper>
